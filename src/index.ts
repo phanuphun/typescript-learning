4b0ac5de023e5639b8a3b3ab095c9deebad89c0f
parnuphun <81858246+parnuphun@users.noreply.github.com>
2024-11-24 19:38:06 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import esmoduleSection from "./course/section2EsModule/esmoduleSection";
-import nameSpaceSection from "./course/section3NameSpace/nameSpaceSection";
-import typeAnnotation from "./course/section4TypeAnnotation/typeAnnotation";
-import section5Guard from "./course/section5TypeGuard/guard";
-import section5Assert from "./course/section5TypeGuard/assert";
-import section8Oop from "./course/section7OOP/oop"
-import generics from "./course/section8Generics/generics"
-import keyof from "./course/section8Generics/keyof"
-import declearation from "./course/section11DeclarationFiles/declearation";
+import ex2 from './fundamental/promise/exmaple5Race'
 
-// console.log(esmoduleSection);
-// console.log(nameSpaceSection);
-// console.log(typeAnnotation);
-// console.log(section5Guard);
-// console.log(section5Assert);
-// console.log(section8Oop);
-// console.log(generics);
-// console.log(keyof);
-console.log(declearation)
-
-
+console.log(ex2);
